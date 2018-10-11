ccae7200c14a7a7e3c984eb4beec54492487c090
u8y <mail.address.is.unsteady@gmail.com>
2018-10-11 17:44:32 +0900

    [WIP] 第2弾

--- src/main/scala/com/example/manything/ambientendre/outsiders/play/controllers/PublisherController.scala
+++ src/main/scala/com/example/manything/ambientendre/outsiders/play/controllers/PublisherController.scala
@@ -1,15 +1,22 @@
 package com.example.manything.ambientendre.outsiders.play.controllers
 
+import java.util.UUID
+
 import com.example.manything.ambientendre.domain.publisher.{
   Publisher,
   PublisherId
 }
 import com.example.manything.ambientendre.usecases.publisher.PublisherUseCases
+import com.example.manything.roundelayout.domain.Identifiability
 import javax.inject._
 import play.api.mvc._
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.format.Formatter
+import play.api.i18n.I18nSupport
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -17,7 +24,8 @@ import scala.concurrent.ExecutionContext.Implicits.global
  */
 @Singleton
 class PublisherController(cc: ControllerComponents, uc: PublisherUseCases)
-  extends AbstractController(cc) {
+  extends AbstractController(cc)
+  with I18nSupport {
 
   /**
    * Create an Action to render an HTML page.
@@ -33,6 +41,33 @@ class PublisherController(cc: ControllerComponents, uc: PublisherUseCases)
   }
 
   def create() = Action { implicit request: Request[AnyContent] =>
+    import play.api.data.format.Formats._
+
+    val i =
+      (id: String) => Identifiability[UUID, Publisher](UUID.fromString(id))
+
+    val formatter = new Formatter[PublisherId] {
+      override def bind(
+        key: String,
+        data: Map[String, String]): Either[Seq[FormError], PublisherId] = {
+        parsing(i, "error.id", Nil)(key, data)
+      }
+      override def unbind(key: String,
+                          value: PublisherId): Map[String, String] = {
+        Map(key -> value.value.toString)
+      }
+    }
+
+    val f = Form(
+      mapping(
+        "identity" -> optional(of[PublisherId](formatter)),
+        "name" -> nonEmptyText
+      )(Publisher.apply)(Publisher.unapply))
+
+    Ok(views.html.publisher.create(f))
+  }
+
+  def performCreation() = Action { implicit request: Request[AnyContent] =>
     Ok("")
   }
 }
---
+++ src/main/scala/com/example/manything/ambientendre/outsiders/play/forms/package.scala
@@ -0,0 +1,3 @@
+package com.example.manything.ambientendre.outsiders.play
+
+package object forms {}
