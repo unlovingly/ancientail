d73650faa3b5e195b70da9a95a7245c53a9d0190
u8y <mail.address.is.unsteady@gmail.com>
2018-11-03 05:11:35 +0900

    第二弾

--- src/main/scala/com/example/manything/ancientail/usecases/slip/StoringProducts.scala
+++ src/main/scala/com/example/manything/ancientail/usecases/slip/StoringProducts.scala
@@ -1,7 +1,6 @@
 package com.example.manything.ancientail.usecases.slip
 
 import com.example.manything.EitherAppliedFuture
-import com.example.manything.ancientail.domain.shop.ShopId
 import com.example.manything.ancientail.domain.slip.Slip
 
 /**
@@ -13,11 +12,9 @@ trait StoringProducts { this: SlipUseCases =>
    * 1. 伝票を保存して
    * 2. 在庫情報を更新する
    */
-  def storing(shopId: ShopId, slip: Slip): EitherAppliedFuture[Slip] = {
-    val productIds = slip.items.map { i =>
-      i.productId
-    }
-    val shop = shops.retrieveWithStocks(shopId, productIds)
+  def storing(slip: Slip): EitherAppliedFuture[Slip] = {
+    val productIds = slip.items.map(_.productId)
+    val shop = shops.retrieveWithStocks(slip.receiverId, productIds)
     // 1. 伝票を保存して
     val result = slips.store(slip)
 
