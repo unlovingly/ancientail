6147cde472de3f8977fbb92840ecab7c149ebcc9
u8y <mail.address.is.unsteady@gmail.com>
2018-11-24 15:20:28 +0900

    fixed migration issues

--- database/migrations/V2018111222474568911400__create_stocks.sql
+++ database/migrations/V2018111222474568911400__create_stocks.sql
@@ -1,5 +1,5 @@
 create table stocks (
-  plu_code varchar(11) primary key,
+  plu_code serial primary key,
   shop_id uuid not null references shops,
   product_id uuid not null references products,
   amount integer not null check (amount > 0),
