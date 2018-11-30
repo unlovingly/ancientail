ef1ee8b6baa582c0cf69fc1fad207d1328b1d238
u8y <mail.address.is.unsteady@gmail.com>
2018-11-30 18:52:25 +0900

    updated migrations

--- database/migrations/V2018111222474568911400__create_stocks.sql
+++ database/migrations/V2018111222474568911400__create_stocks.sql
@@ -1,5 +1,5 @@
 create table stocks (
-  plu_code serial primary key,
+  plu_code text primary key,
   shop_id uuid not null references shops,
   product_id uuid not null references products,
   amount integer not null check (amount > 0),
--- database/migrations/V2018111222482334570900__create_slips.sql
+++ database/migrations/V2018111222482334570900__create_slips.sql
@@ -5,7 +5,8 @@ create table exchange_slips (
   sender_id uuid not null references shops,
   receiver_id uuid not null references shops,
   approved_at timestamp with time zone not null,
-  published_at timestamp with time zone not null
+  published_at timestamp with time zone not null,
+  unique (number, sender_id)
 );
 create table purchase_slips (
   slip_id uuid primary key default gen_random_uuid(),
@@ -14,7 +15,8 @@ create table purchase_slips (
   sender_id uuid not null references publishers,
   receiver_id uuid not null references shops,
   approved_at timestamp with time zone not null,
-  published_at timestamp with time zone not null
+  published_at timestamp with time zone not null,
+  unique (number, sender_id)
 );
 create table sales_slips (
   slip_id uuid primary key default gen_random_uuid(),
@@ -23,7 +25,8 @@ create table sales_slips (
   sender_id uuid not null references shops,
   receiver_id uuid null references shops,
   approved_at timestamp with time zone not null,
-  published_at timestamp with time zone not null
+  published_at timestamp with time zone not null,
+  unique (number, sender_id)
 );
 
 comment on column sales_slips.receiver_id is 'receiver_id はあとから設定されることがあるので null をとる'
