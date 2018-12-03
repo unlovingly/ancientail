99f9842d3896ad5546a975de39480d15285264ec
u8y <mail.address.is.unsteady@gmail.com>
2018-12-03 23:04:10 +0900

    おかしい制約をなおした

--- database/migrations/V2018111222474568911400__create_stocks.sql
+++ database/migrations/V2018111222474568911400__create_stocks.sql
@@ -2,6 +2,6 @@ create table stocks (
   plu_code text primary key,
   shop_id uuid not null references shops,
   product_id uuid not null references products,
-  amount integer not null check (amount > 0),
-  price integer not null check (price > 0)
+  amount integer not null check (amount >= 0),
+  price integer not null check (price >= 0)
 )
