f411c1770cb639fa6ac779ecf0a0c1e4461c67ed
u8y <mail.address.is.unsteady@gmail.com>
2019-01-01 16:48:57 +0900

    implement store(Seq[SalesSlip])

--- src/main/scala/com/example/manything/ancientail/domain/models/slip/sales/SalesSlipRepository.scala
+++ src/main/scala/com/example/manything/ancientail/domain/models/slip/sales/SalesSlipRepository.scala
@@ -4,4 +4,11 @@ import com.example.manything.ancientail.domain.models.slip.SlipRepository
 
 trait SalesSlipRepository[A[_]] extends SlipRepository[SalesSlip, A] {
   override type EntityType = SalesSlip
+
+  /**
+   * 売上を複数登録する
+   * SlipRepository でもいいが他の伝票はできなくてもいい
+   * @see store(entity: EntityType): A[SlipType]
+   */
+  def store(entity: Seq[EntityType]): A[Seq[SalesSlip]]
 }
